f61b6082a3c4f2186cd17839fbd4c70040f26baf
abdulbasit <ba3568008@gmail.com>
2023-05-29 21:11:34 +0500

    updated slider

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -14,18 +14,18 @@ function urlFor(source: any) {
 
 const Card: FC<{ singleProductData: oneProductType }> = ({ singleProductData }) => {
     return (
-        <Link href={`/catalog/${singleProductData.slug.current}`}>
-            <div className='max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300'>
-                <div className='relative w-full'>
-                    <div className='absolute inset-0 z-10' />
-                    <Image width={1000} height={1000} src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} />
-                </div>
-                <div className='space-y-1 text-gray-600 font-semibold text-lg select-none'>
+        <div className='max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300'>
+            <div className='relative w-full'>
+                <div className='absolute inset-0 z-10' />
+                <Image width={1000} height={1000} src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} />
+            </div>
+            <div className='space-y-1 text-gray-600 font-semibold text-lg select-none'>
+                <Link href={`/catalog/${singleProductData.slug.current}`}>
                     <h6>{singleProductData.productName}</h6>
                     <p>${singleProductData.price}</p>
-                </div>
+                </Link>
             </div>
-        </Link>
+        </div>
     )
 }
 
