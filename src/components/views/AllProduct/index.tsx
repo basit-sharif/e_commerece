ff1caa0c11fa75702a4608b758c7a303dc36a09d
abdulbasit <ba3568008@gmail.com>
2023-05-30 01:00:17 +0500

    solving issues

--- src/components/views/AllProduct/index.tsx
+++ src/components/views/AllProduct/index.tsx
@@ -9,10 +9,11 @@ interface propsType {
 export default class AllProductsCompo extends Component<{ ProdutcData: propsType }> {
     getData = () => {
         console.log(this.props.ProdutcData.productArray[0].price)
+        console.log(this.props.ProdutcData)
     }
     render() {
         return (
-            <div onClick={this.getData}>{this.props.ProdutcData.productArray[0].price}</div>
+            <div onClick={this.getData}>HI</div>
         )
     }
 }
