eb3ca8c77582c4515a134eff8ba3e960d671cb6a
abdulbasit <ba3568008@gmail.com>
2023-05-30 15:59:18 +0500

    solved some isseus

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -10,12 +10,17 @@ async function getAllProductsForSearch() {
 const Search = async ({ params }: { params: { query: string } }) => {
     let slug = (params.query).toLowerCase();
     let data = await getAllProductsForSearch()
-    let dataToMap = await data.filter((item: oneProductType) => ((item.productName).toLowerCase()).indexOf(slug));
+    let dataToMap = await data.filter((item: oneProductType) => {
+        if ((item.productName).toLowerCase().indexOf(slug) >= 0) {
+            return true
+        }
+        return false
+    });
     return (
         <div
             className="grid grid-cols-1 md:grid-cols-2 py-10 lg:grid-cols-3 gap-4"
         >
-            {dataToMap && dataToMap.result.map((items: oneProductType, index: number) => (
+            {dataToMap && dataToMap.map((items: oneProductType, index: number) => (
                 <Card key={index} singleProductData={items} />
             ))}
         </div>
