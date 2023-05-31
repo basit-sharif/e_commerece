a114ac3f1dbfdfa1e7a2bd008844154d727808aa
abdulbasit <ba3568008@gmail.com>
2023-05-31 19:11:09 +0500

    solved errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <Wrapper>
           <Navbar />
-            {/* <div className='min-h-screen'>
-              {children}
-            </div>
-          <Footer /> */}
+          <div className='min-h-screen'>
+            {children}
+          </div>
+          <Footer />
         </Wrapper>
       </body>
     </html>
