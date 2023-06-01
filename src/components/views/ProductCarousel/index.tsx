99b032ab3b5971cf714af46629c0a8029865a2e2
abdulbasit <ba3568008@gmail.com>
2023-06-01 23:19:17 +0500

    solved errors

--- src/components/views/ProductCarousel/index.tsx
+++ src/components/views/ProductCarousel/index.tsx
@@ -1,42 +1,7 @@
 "use client"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import { oneProductType } from "@/components/utils/ProductsDataArrayAndType"
 import { Component, FC, ReactNode } from "react"
 import Card from "../Card"
-
-// export default class ProductCarousel extends Component<{ ProductData: Array<oneProductType> }> {
-
-//     render(): ReactNode {
-//         const settings = {
-//             speed: 500,
-//             slidesToShow: 3,
-//             slidesToScroll: 1,
-//             responsive: [
-//                 {
-//                     breakpoint: 824,
-//                     settings: {
-//                         slidesToShow: 1,
-//                         slidesToScroll: 1,
-//                     }
-//                 }
-//             ]
-//         };
-
-//         return (
-//             <Slider {...settings}>
-//                 {this.props.ProductData.map((item: oneProductType, index: number) => (
-//                     <Card singleProductData={item} />
-//                 ))}
-//             </Slider>
-//         )
-//     }
-// }
-
-
-
-
 
 const ProductCarousel: FC<{ ProductData: Array<oneProductType> }> = ({ ProductData }) => {
     let initialX: number;
