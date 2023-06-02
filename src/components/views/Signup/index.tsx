9366304c6d5c77f44ac079e218bb287d3267c461
abdulbasit <ba3568008@gmail.com>
2023-06-02 13:08:50 +0500

    maded signup form

--- src/components/views/Signup/index.tsx
+++ src/components/views/Signup/index.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { cartContext } from '@/global/context';
+import Link from 'next/link';
 import { useContext, useState } from 'react';
 
 type SignupFormData = {
@@ -60,14 +61,14 @@ const SignupFormComp = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <div className="bg-white shadow-md rounded px-8 py-6">
+            <div className="bg-slate-100 shadow-2xl border-t-8 border-pink-700 rounded px-8 pt-6 pb-8">
                 <h2 className="text-2xl font-bold mb-4">Signup</h2>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullName">
                         Full Name
                     </label>
                     <input
-                        className={`appearance-none border rounded w-full md:w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.fullName ? 'border-red-500' : ''
+                        className={`appearance-none border border-purple-300 rounded w-full md:w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.fullName ? 'border-red-500' : ''
                             }`}
                         type="text"
                         name="fullName"
@@ -85,7 +86,7 @@ const SignupFormComp = () => {
                         Email
                     </label>
                     <input
-                        className={`appearance-none border rounded w-full md:w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.email ? 'border-red-500' : ''
+                        className={`appearance-none border border-purple-300 rounded w-full md:w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.email ? 'border-red-500' : ''
                             }`}
                         type="email"
                         name="email"
@@ -94,15 +95,14 @@ const SignupFormComp = () => {
                         onChange={handleInputChange}
                         placeholder="Enter your email"
                     />
-                    {errors.email && <p className="text-red-500 text-xs
-            italic">{errors.email}</p>}
+                    {errors.email && <p className="text-red-500 text-xs italic">{errors.email}</p>}
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
                     <input
-                        className={`appearance-none border rounded w-full md:w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.password ? 'border-red-500' : ''
+                        className={`appearance-none border border-purple-300 rounded w-full md:w-96 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.password ? 'border-red-500' : ''
                             }`}
                         type="password"
                         name="password"
@@ -115,9 +115,10 @@ const SignupFormComp = () => {
                         <p className="text-red-500 text-xs italic">{errors.password}</p>
                     )}
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-between">
+                    <Link className='text-blue-400 text-sm' href={"/login"}>Already have an account?</Link>
                     <button
-                        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-5 rounded focus:outline-none focus:shadow-outline"
                         type="button"
                         onClick={handleSignup}
                     >
